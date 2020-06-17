09fbb0c4e9d5cd38170ecd7834c99a05a3c1cd9c
dan <dan@nod-labs.com>
2020-06-16 17:26:03 -0700

    [cmake] Added the option to include external deps

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -1,5 +1,4 @@
 set(protobuf_BUILD_TESTS OFF CACHE BOOL "Disable tests for protobuf")
-
 set(BUILD_SHARED_LIBS OFF)
 set(LIBRARY_POLICY STATIC)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../protobuf/cmake ${CMAKE_CURRENT_BINARY_DIR}/protobuf)
